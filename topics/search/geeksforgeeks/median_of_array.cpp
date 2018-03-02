5890f745be331278c4fe04d3c338c04a01b23c32
aashan007 <sarathchandran1995@gmail.com>
2018-03-02 18:53:17 +0000

    added median problem

---
+++ topics/search/geeksforgeeks/median_of_array.cpp
@@ -0,0 +1,27 @@
+#include<iostream>
+#include<bits/stdc++.h>
+using namespace std;
+
+
+int main(){
+  int t;
+  cin>>t;
+  while(t--){
+    int n;
+    cin>>n;
+    int a[n];
+    for(int i=0;i<n;i++){
+      cin>>a[i];
+    }
+    sort(a,a+n);
+
+    int median;
+    if(n%2==0){
+       median=(a[n/2]+a[(n-1)/2])/2;
+    }
+    else{
+     median=a[n/2];
+    }
+    cout<<median;
+  }
+}
