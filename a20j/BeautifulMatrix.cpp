b44929ef08cb88fc54502a8182401faa13290c86
I520664 <sarathchandran.s@sap.com>
2020-01-10 18:06:44 +0530

    beautiful matrix

---
+++ a20j/BeautifulMatrix.cpp
@@ -0,0 +1,23 @@
+#include<bits/stdc++.h>
+using namespace std;
+int main(){
+    int a[5][5];
+    for(int i=0;i<5;i++){
+        for(int j=0;j<5;j++){
+            cin>>a[i][j];
+        }
+    }
+    int res=0;
+    for(int i=0;i<5;i++){
+        for(int j=0;j<5;j++){
+            if(a[i][j]){
+                res=abs(i-2)+abs(j-2);
+                break;
+            }
+        }
+        if(res){
+            break;
+        }
+    }
+    cout<<res<<endl;
+}
