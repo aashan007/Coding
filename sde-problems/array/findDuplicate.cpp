68e155c1bfb9095c2a15a3314609d74677ada35a
aashan007 <sarathchandran1995@gmail.com>
2020-11-01 16:08:46 +0530

    sort 0,1,2

--- sde-problems/array/findDuplicate.cpp
+++ sde-problems/array/findDuplicate.cpp
@@ -1,15 +1,21 @@
 class Solution {
 public:
     int findDuplicate(vector<int>& nums) {
-        int val=0;
-        for(int i=1;i<nums.size();i++){
-            val^=i;
+        int n = nums.size();
+        int max_idx = 0;
+        int curr_max = 0;
+        for(int i=0;i<n;++i){
+            int id=nums[i]%n;
+            nums[id] +=n;
         }
-        for(int i=0;i<nums.size();i++){
-            val^=nums[i];
+        for(int i=0;i<n;++i){
+            if(nums[i]>curr_max){
+                curr_max = nums[i];
+                max_idx=i;
+            }
+            nums[i]%=n;
         }
-        
-        return val;
-        
+        return max_idx;
+            
     }
 };
---
+++ sde-problems/array/sort012.cpp
@@ -0,0 +1,27 @@
+class Solution {
+public:
+    void sortColors(vector<int>& nums) {
+        int i = 0;
+        int k = 0;
+        int j = nums.size()-1;
+        while(i<j && k<=j && k>=0){
+             if(nums[k]==1){
+                 k++;
+             }
+             else if(nums[k]==0){
+                 int temp = nums[k];
+                 nums[k] = nums[i];
+                 nums[i] = temp;
+                 i++;
+                 k=i;
+             }
+             else{
+                 int temp =nums[k];
+                 nums[k] = nums[j];
+                 nums[j] = temp;
+                 j--;
+             }
+        }
+        
+    }
+};
