39c9cfe560a7ee1c8779e53bfa437a0960bee8c0
aashan007 <sarathchandran1995@gmail.com>
2020-11-06 22:11:54 +0530

    minimumcostchips

---
+++ challenges/LeetCode30DayChallenge-Nov/5.minimumCostChips.cpp
@@ -0,0 +1,22 @@
+class Solution {
+public:
+    int minCostToMoveChips(vector<int>& position) {
+        int even=0;
+        int odd=0;
+        for(int i=0;i<position.size();i++){
+            if(position[i]%2==0){
+                odd++;
+            }
+            else{
+                even++;
+            }
+        }
+        
+        if(even==position.size() || odd==position.size()){
+            return 0;
+        }
+        
+        
+        return min(even,odd);
+    }
+};
