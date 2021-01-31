cb378740a300b20b97957446037152c3858f2d42
aashan007 <sarathchandran1995@gmail.com>
2021-01-31 18:48:40 +0530

    remove duplicate from sorted lst

---
+++ challenges/LeetCode30DayChallenge-January-2021/5.removeDuplicateFromSortedList.cpp
@@ -0,0 +1,42 @@
+/**
+ * Definition for singly-linked list.
+ * struct ListNode {
+ *     int val;
+ *     ListNode *next;
+ *     ListNode() : val(0), next(nullptr) {}
+ *     ListNode(int x) : val(x), next(nullptr) {}
+ *     ListNode(int x, ListNode *next) : val(x), next(next) {}
+ * };
+ */
+class Solution
+{
+public:
+    ListNode *deleteDuplicates(ListNode *head)
+    {
+        if (head == NULL || head->next == NULL)
+            return head;
+        ListNode dummy;
+        dummy.next = head;
+        ListNode *prev = head, *node = head->next, *curr = &dummy;
+        while (node)
+        {
+            if (prev->val == node->val)
+            {
+                while (node && prev->val == node->val)
+                {
+                    node = node->next;
+                }
+                curr->next = node;
+                if (node == NULL)
+                    break;
+            }
+            else
+            {
+                curr = prev;
+            }
+            prev = node;
+            node = node->next;
+        }
+        return dummy.next;
+    }
+};
