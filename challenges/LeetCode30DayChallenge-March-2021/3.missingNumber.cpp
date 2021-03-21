c1898bbd9e8e2d34ffd383adeb3b40cbb2bb8ee0
aashan007 <sarathchandran1995@gmail.com>
2021-03-21 20:21:09 +0530

    missing numbeR

---
+++ challenges/LeetCode30DayChallenge-March-2021/3.missingNumber.cpp
@@ -0,0 +1,15 @@
+class Solution
+{
+public:
+    int missingNumber(vector<int> &nums)
+    {
+        int n = nums.size();
+
+        int result = n * (n + 1) / 2;
+
+        for (int i = 0; i < nums.size(); i++)
+            result -= nums[i];
+
+        return result;
+    }
+};
